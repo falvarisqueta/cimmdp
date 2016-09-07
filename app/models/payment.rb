df3eab4b1b77f7b91470cbf5b5c75c0a21d6bcce
Francisco Alvarisqueta <francisco.alvarisqueta@eclinicalos.com>
2016-09-07 15:06:03 -0300

    Add user to payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,7 @@ class Payment < ActiveRecord::Base
   belongs_to :visit
   belongs_to :doctor
   belongs_to :patient
+  belongs_to :user
 
-  validates :patient_id, :doctor_id, :visit_id, presence: true
+  validates :patient_id, :user_id, :visit_id, presence: true
 end
