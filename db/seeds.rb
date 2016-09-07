53e57e7c4a2c854cb7dd449486242f542334b044
Francisco Alvarisqueta <francisco.alvarisqueta@eclinicalos.com>
2016-09-07 15:10:57 -0300

    Set id to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-VisitType.create(name: 'Screening')
-VisitType.create(name: 'Rando')
-VisitType.create(name: 'Phone')
-VisitType.create(name: 'Presence')
+VisitType.create(id:1, name: 'Screening')
+VisitType.create(id:2, name: 'Rando')
+VisitType.create(id:3, name: 'Phone')
+VisitType.create(id:4, name: 'Presence')
