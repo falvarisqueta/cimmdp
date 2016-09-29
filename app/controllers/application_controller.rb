4e0ef483cfb698b5aa421711b2b98dd24d3241b9
Francisco Alvarisqueta <francisco.alvarisqueta@eclinicalos.com>
2016-09-29 15:53:30 -0300

    Fix logout finance person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :finance_person?
   add_flash_types :error
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def finance_person?
+    !current_user.nil? && current_user.finance_person?
   end
 
 end
