205a4308b3da7f7701d48cef0ea3b2f32a81cd14
Francisco Alvarisqueta <francisco.alvarisqueta@eclinicalos.com>
2016-09-29 14:50:13 -0300

    Agregar consultorios por piso

--- app/models/concerns/appointment_room.rb
+++ app/models/concerns/appointment_room.rb
@@ -1,10 +1,14 @@
 module AppointmentRoom
   extend ActiveSupport::Concern
 
-  Consulting = StaticValue.new(1, 'Consultorio')
-  Analysis = StaticValue.new(2, 'Análisis')
-  Electro = StaticValue.new(3, 'Electro')
-  Spiro = StaticValue.new(4, 'Espiro')
+  Consulting31 = StaticValue.new(1, 'Piso 1 - Consultorio 3')
+  Consulting41 = StaticValue.new(2, 'Piso 1 - Consultorio 4')
+  Consulting51 = StaticValue.new(3, 'Piso 1 - Consultorio 5')
+  Consulting25 = StaticValue.new(4, 'Piso 5 - Consultorio 2')
+  Consulting35 = StaticValue.new(5, 'Piso 5 - Consultorio 3')
+  Analysis = StaticValue.new(6, 'Análisis')
+  Electro = StaticValue.new(7, 'Electro')
+  Spiro = StaticValue.new(8, 'Espiro')
 
   Collection = constants.map { |appointment_room| const_get(appointment_room) }
   All = Collection.map(&:id)
